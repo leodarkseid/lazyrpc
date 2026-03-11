6775ca028446b6009b7b1a28afabefb4108cbe2a
leodarkseid <leodarkseid@gmail.com>
2026-03-11 11:46:34 +0100

    test: use native node buffer blobs instead of circular undici require

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,10 +1,14 @@
-const { File, Blob, FormData } = require('undici');
-if (typeof global.File === 'undefined') {
-  global.File = File || class File { };
-}
+// Node 18+ natively supports Blob and File via the buffer module
+const { Blob, File } = require('buffer');
+// FormData is natively available globally in Node 18+, but if Jest stripped it:
+const { FormData } = require('undici'); // Wait, FormData is safe from undici, but File/Blob were the crashes.
+
 if (typeof global.Blob === 'undefined') {
-  global.Blob = Blob || class Blob { };
+  global.Blob = Blob || require('node:buffer').Blob;
+}
+if (typeof global.File === 'undefined') {
+  global.File = File || require('node:buffer').File;
 }
 if (typeof global.FormData === 'undefined') {
-  global.FormData = FormData || class FormData { };
+  global.FormData = typeof FormData !== 'undefined' ? FormData : class FormData {};
 }
